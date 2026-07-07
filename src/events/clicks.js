bf250c33405653f7359c892d4c23e8e8ef0804e3
Zetsukae <dangeloalexis2@gmail.com>
2026-07-07 08:30:24 +0200

    feat(clicks): add first function

--- src/events/clicks.js
+++ src/events/clicks.js
@@ -4,7 +4,10 @@
  * The alert text should contain "Button clicked"
  */
 export function reactToClickEvent() {
-  // Write your code here
+    const button = document.getElementById("click-me");
+    button.addEventListener("click", () => {
+        alert("Button clicked");
+    })
 }
 
 /**
@@ -12,5 +15,5 @@ export function reactToClickEvent() {
  * But this time you should add a new div below the button with the content "clicked"
  */
 export function addEventToDomOnClick() {
-  // Write your code here
+    // Write your code here
 }
